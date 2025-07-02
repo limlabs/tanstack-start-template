46f3ce8a11b10d4669cd87699ae42eeab25112e9
ediazm <alcmaeon@gmail.com>
2025-07-01 19:43:10 -0700

    fixed toml

--- app.config.ts
+++ app.config.ts
@@ -5,16 +5,15 @@ import * as path from 'path'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   build: {
-      manifest: true,
-      assetsDir: 'assets',
-      rollupOptions: {
-        output: {
-          assetFileNames: ({ name }: { name?: string }) => {
-            const baseName = name ? path.parse(name).name : 'asset'
-            return `assets/${baseName}-[hash][extname]`
-          },
-        },
-      },
+    manifest: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: ({ name }: { name?: string }) => {
+          const baseName = name ? path.parse(name).name : 'asset'
+          return `assets/${baseName}-[hash][extname]`
+        }
+      }
     }
   }
 })
